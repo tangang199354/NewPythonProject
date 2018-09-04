841efe144c13e3d761de3204b99a7383be8a14ed
tangang <tangang@ijovo.com>
2018-09-04 13:19:09 +0800

    111

--- cgi-bin/app.py
+++ cgi-bin/app.py
@@ -14,6 +14,9 @@ def init(loop):
     app.router.add_route('GET', '/', index)
     srv = yield from loop.create_server(app.make_handler(), '127.0.0.1', 9000)
     logging.info('server started at http://127.0.0.1:9000...')
+    
+    
+    
     return srv
 
 loop = asyncio.get_event_loop()
